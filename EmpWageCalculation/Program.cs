ca54c7992b885647b32512e44883b3a7930ef9e6
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-29 13:45:43 +0530

    [Ritesh]add : Using Switch Case Statment

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -8,30 +8,30 @@ namespace EmpWageCalculation
 {
     public class Program
     {
+        public const int IS_FULL_TIME = 1; 
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            int IS_FULL_TIME = 1;    //Constant
-            int  IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);   // 0 or 1
-            if(empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                Console.WriteLine("Full Time Employee Is Present");   //1-->Emp is present full time
-                empHrs = 8;
-            }
-            else if(empCheck == IS_PART_TIME)
-            {
-                Console.WriteLine("Half Time Employee Is Present");  //2--> Emp is Present Half Time
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");   //0 -->Emp Is Absent
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    Console.WriteLine("Full Time Employee Is Present");   //1-->Emp is present full time
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("Half Time Employee Is Present");  //2--> Emp is Present Half Time
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");   //0 -->Emp Is Absent
+                    empHrs = 0;
+                    break;
             }
 
             empWage = EMP_RATE_PER_HOUR * empHrs;
