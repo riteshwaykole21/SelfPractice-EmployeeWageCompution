49f6e56cd59e24e055e50dbed67aead68acc1ff3
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-29 12:50:39 +0530

    [Ritesh]add : Check Employee Attendances

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -11,7 +11,18 @@ namespace EmpWageCalculation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            Console.ReadLine(); 
+            int IS_FULL_TIME = 1;               //Constant
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);   // 0 or 1
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");   //1-->Emp is Present
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");   //0 -->Emp Is Absent
+            }
+            Console.ReadLine();
         }
     }
 }
