8ad0be635a01dd50a93cd5adb93565126ca359b8
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-29 13:24:54 +0530

    [Ritesh]add : Add Part Time Employee Wage

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -11,22 +11,29 @@ namespace EmpWageCalculation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            int IS_FULL_TIME = 1;               //Constant
+            int IS_FULL_TIME = 1;    //Constant
+            int  IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);   // 0 or 1
+            int empCheck = random.Next(0, 3);   // 0 or 1
             if(empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");   //1-->Emp is Present
+                Console.WriteLine("Full Time Employee Is Present");   //1-->Emp is present full time
                 empHrs = 8;
+            }
+            else if(empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Half Time Employee Is Present");  //2--> Emp is Present Half Time
+                empHrs = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");   //0 -->Emp Is Absent
                 empHrs = 0;
             }
+
             empWage = EMP_RATE_PER_HOUR * empHrs;
             Console.WriteLine("Employee Wage : " +empWage);
             Console.ReadLine();
