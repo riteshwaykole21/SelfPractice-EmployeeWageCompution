1b6390aaa23aa6cc97f78fb95455d744a881f0b7
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-29 13:05:28 +0530

    [Ritesh]add : calculate Daily Employee wage

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -12,16 +12,23 @@ namespace EmpWageCalculation
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             int IS_FULL_TIME = 1;               //Constant
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);   // 0 or 1
             if(empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");   //1-->Emp is Present
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");   //0 -->Emp Is Absent
+                empHrs = 0;
             }
+            empWage = EMP_RATE_PER_HOUR * empHrs;
+            Console.WriteLine("Employee Wage : " +empWage);
             Console.ReadLine();
         }
     }
