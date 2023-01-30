545322e9736a49bd5bb2b7132234c08562429d05
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-30 10:22:58 +0530

    [Ritesh]add : Monthly Wage With Condition UC

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -11,31 +11,40 @@ namespace EmpWageCalculation
         public const int IS_FULL_TIME = 1; 
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTHS = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);   // 0 or 1
-            switch (empCheck)
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while(totalEmpHrs <= MAX_HRS_IN_MONTHS && totalWorkingDays < NUM_OF_WORKING_DAYS )
             {
-                case IS_FULL_TIME:
-                    Console.WriteLine("Full Time Employee Is Present");   //1-->Emp is present full time
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    Console.WriteLine("Half Time Employee Is Present");  //2--> Emp is Present Half Time
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");   //0 -->Emp Is Absent
-                    empHrs = 0;
-                    break;
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);   // 0 or 1
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        Console.WriteLine("Full Time Employee Is Present");   //1-->Emp is present full time
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        Console.WriteLine("Half Time Employee Is Present");  //2--> Emp is Present Half Time
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");   //0 -->Emp Is Absent
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:"+totalWorkingDays +" " + "Working Hour :"+empHrs);
+                
             }
-
-            empWage = EMP_RATE_PER_HOUR * empHrs;
-            Console.WriteLine("Employee Wage : " +empWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine($"totalEmpHrs :{totalEmpHrs} * EMP_RATE_PER_HOUR :{EMP_RATE_PER_HOUR}=totalEmpWage :{totalEmpWage}");
             Console.ReadLine();
         }
     }
